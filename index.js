ff03aa7023f012b0a5d3c0d7f083f64d3306ca67
Showrab15 <supto50showrab@gmail.com>
2023-05-02 12:12:45 +0600

    vercel json file created for deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+// port of my server
 const port =  5000;
 
 const recipes = require('./data/recipes.json');
